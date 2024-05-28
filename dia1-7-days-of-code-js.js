f97a3a2b29bda9895b0ce275b24786a1753c35f7
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-05-28 07:47:41 -0300

    Add files via upload

---
+++ dia1-7-days-of-code-js.js
@@ -0,0 +1,26 @@
+let NumeroUm        = 1
+let StringUm        = '1'
+let NumeroTrinta    = 30
+let StringTrinta    = '30'
+let NumeroDez       = 10
+let StringDez       = '10'
+
+if (NumeroUm == StringUm){
+    console.log('As variáveis NumeroUm e StringUm tem o mesmo valor, mas tipos diferentes.')
+
+}else{
+    console.log('As variáveis NumeroUm e StringUm não tem o memso valor.')
+}
+
+if (NumeroTrinta == StringTrinta){
+    console.log('As variáveis NumeroTrinta e StringTrinta tem o mesmo valor e mesmo tipo.')
+
+}else{
+    console.log('As variáveis NumeroTrinta e StringTrinta não tem o mesmo tipo.')
+}
+
+if (NumeroDez == StringDez){
+    console.log('As variáveis NumeroDez e StringDez tem o mesmo valor, mas tipos diferentes.')
+}else{
+    console.log('As variáveis NumeroDez e StringDez não tem o mesmo valor.')
+}
