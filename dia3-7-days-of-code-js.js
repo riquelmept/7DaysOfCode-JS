d86cc6c57228f2193315c55eb24804d695a53d16
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-05-30 11:25:27 -0300

    Update dia3-7-days-of-code-js.js
    Solução do dia 3.

--- dia3-7-days-of-code-js.js
+++ dia3-7-days-of-code-js.js
@@ -1,81 +1,28 @@
-const userName = prompt('Qual é o seu nome?');
-let area_dev = '';
+const area = prompt("Você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área:");
+let linguagem = "";
+if (area === "Front-End"){
+    linguagem = prompt("Você quer aprender React ou Vue?");
+}
+else if (area === "Back-End"){
+    linguagem = prompt("Você quer aprender C# ou Java?");
+}
+else {
+    alert("Você não inseriu uma área válida!");
+}
 
-while (true) {
-    area_dev = prompt('Você deseja seguir área de Front-End ou Back-End? Digite 1 para Front-End ou 2 para Back-End:');
+const especialidadeOuFullstack = prompt("Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack");
+if (especialidadeOuFullstack == 1){
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+}
+else if (especialidadeOuFullstack == 2){
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+}
+else {
+    alert("Você não inseriu um valor válido!");
+}
 
-    if (area_dev === '1') {
-        area_dev = 'Front-End';
-        break;
-    } else if (area_dev === '2') {
-        area_dev = 'Back-End';
-        break;
-    } else {
-        alert('Opção inválida. Por favor, digite 1 para Front-End ou 2 para Back-End.');
-    }
-};
-
-function chooseTechnology(area) {
-    let technology = '';
-    while (true) {
-        if (area === 'Front-End') {
-            technology = prompt(`Aprender ${area}? Digite 1 para React ou 2 para Vue:`).toLowerCase();
-            if (technology === '1') {
-                return 'React';
-            } else if (technology === '2') {
-                return 'Vue';
-            } else {
-                alert('Opção inválida, por favor digite 1 para React ou 2 para Vue.');
-            }
-        } else if (area === 'Back-End') {
-            technology = prompt(`Aprender ${area}? Digite 1 para C# ou 2 para Java:`).toLowerCase();
-            if (technology === '1') {
-                return 'C#';
-            } else if (technology === '2') {
-                return 'Java';
-            } else {
-                alert('Opção inválida, por favor digite 1 para C# ou 2 para Java.');
-            }
-        }
-    };
-};
-
-let devTechnology = chooseTechnology(area_dev);
-
-let specializationOrFullstack = '';
-
-while (true) {
-    specializationOrFullstack = prompt(`${userName}, você deseja se especializar na área escolhida ou se tornar Fullstack? Digite 1 para especialização e 2 para Fullstack.`);
-    if (specializationOrFullstack === '1' || specializationOrFullstack === '2') {
-        break;
-    } else {
-        alert('Opção inválida, tente novamente!');
-    };
-};
-
-if (specializationOrFullstack === '1') {
-    alert(`Parabéns, ${userName}! Você escolheu se especializar na área de ${devArea}. Continue aprendendo e se aprimorando!`);
-} else if (specializationOrFullstack === '2') {
-    alert(`Excelente escolha, ${userName}! Ser Fullstack permitirá que você trabalhe em diversas áreas e tenha um conhecimento abrangente.`);
-};
-
-const technologies = [];
-
-let moreTechnologies = '';
-
-while (true) {
-    moreTechnologies = prompt('Tem mais alguma tecnologia que você gostaria de aprender? Digite "Sim" ou "Não".').toLowerCase();
-    if (!['sim', 's', 'não', 'nao', 'n'].includes(moreTechnologies)) {
-        alert('Opção Inválida');
-    } else if (moreTechnologies === 'sim' || moreTechnologies === 's') {
-        let newTech = prompt('Qual?');
-        alert(`${newTech} é realmente uma tecnologia muito legal!`);
-        technologies.push(newTech);
-    } else if (moreTechnologies === 'não' || moreTechnologies === 'nao' || moreTechnologies === 'n') {
-        break;
-    };
-};
-
-alert('Ok, até a próxima!');
-
-console.log(`${userName}, você escolheu aprender as seguintes tecnologias: ${devTechnology}, ${technologies.join(', ')}`);
+let msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+while (msg === "ok"){
+    let novaTecnologia = prompt("Qual?");
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`)
+    msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
